a05beaa18feb5a7142373c1138678b1ba1f4a58e
hymen81 <hymen81@hotmail.com>
2015-10-28 17:09:25 +0800

    幫助工廠選bat的tool

---
+++ OneBIOS_SKUID_Selection/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OneBIOS_SKUID_Selection
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int QUEST_SIZE = 11;
+        int QuestionScore;
+        List<Question> questions = new List<Question>();
+        List<string> ansLabels;
+        Stack<int> undoStack = new Stack<int>();
+        List<List<Selection>> buttonsLabels;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            buttonsLabels = StringData.getButtonData;
+            ansLabels = StringData.getAnsData;
+
+            List<string> questionLabels = StringData.getQuestionData;
+      
+            for (int i = 0; i < QUEST_SIZE; i++)      
+                questions.Add(new Question(i, questionLabels[i], buttonsLabels[i]));
+           
+            this.QuestionScore = 0;
+            this.QLabel.Content = questions[this.QuestionScore].QString;
+            this.LButton.Content = questions[this.QuestionScore].ButtonsLabel[(int)BUTTON_POSIOION.left].Label;
+            this.RButton.Content = questions[this.QuestionScore].ButtonsLabel[(int)BUTTON_POSIOION.right].Label;
+            this.MButton.Content = questions[this.QuestionScore].ButtonsLabel[(int)BUTTON_POSIOION.mid].Label;
+            this.MButton.Visibility = Visibility.Visible;
+            this.UndoButton.IsEnabled = false;
+        }
+
+        private void ButtonCheck(BUTTON_POSIOION pos, string ansString)
+        {
+            if (BUTTON_POSIOION.undo == pos)
+            {
+                if (undoStack.Count > 0)
+                {
+                    this.QuestionScore -= undoStack.Pop();//回歸問題tree的位置
+                    for (int i = 0; i < 2; i++)//把Q跟A移除掉
+                        this.QuestionHistroyList.Items.RemoveAt(this.QuestionHistroyList.Items.Count - 1);
+                    this.UndoButton.IsEnabled = undoStack.Count > 0;//防呆 stack的size要大於0才能點
+                }            
+            }
+            else//點下左邊中間右邊的button之後
+            {
+                int score = questions[this.QuestionScore].ButtonsLabel[(int)pos].JumpStep;            
+                if (-1 == score)//代表到了結尾
+                {
+                    MessageBox.Show(ansLabels[questions[this.QuestionScore].ButtonsLabel[(int)pos].AnsID], "Infromation");
+                    return;
+                }
+                undoStack.Push(score);//執行過的放到stack做紀錄
+                this.QuestionHistroyList.Items.Add(this.QLabel.Content);//印出問題
+                this.QuestionHistroyList.Items.Add("A:" + ansString);
+                this.UndoButton.IsEnabled = true;
+                this.QuestionScore += score;
+            }
+            this.LButton.Content = questions[this.QuestionScore].ButtonsLabel[(int)BUTTON_POSIOION.left].Label;
+            this.RButton.Content = questions[this.QuestionScore].ButtonsLabel[(int)BUTTON_POSIOION.right].Label;
+
+            if (questions[this.QuestionScore].ButtonsLabel.Count > 2) 
+            {
+                this.MButton.Content = questions[this.QuestionScore].ButtonsLabel[(int)BUTTON_POSIOION.mid].Label;
+                this.MButton.Visibility = Visibility.Visible;
+            }
+            else
+                this.MButton.Visibility = Visibility.Hidden;
+            this.QLabel.Content = questions[this.QuestionScore].QString;
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCheck(StringData.ButtonType[((Button)sender).Name], ((Button)sender).Content.ToString());
+        }
+    }
+
+    public class Selection
+    {
+        public Selection(string label, int jumpStep, int ansID)
+        {
+            this.label = label;
+            this.jumpStep = jumpStep;
+            this.ansID = ansID;
+        }
+        public string Label { get { return this.label; } }
+        public int JumpStep { get { return this.jumpStep; } }
+        public int AnsID { get { return this.ansID; } }
+
+        private string label;
+        private int jumpStep;
+        private int ansID;
+    }
+
+
+    public class Question
+    {
+        public Question(int id, string qString, List<Selection> buttonsLabel)
+        {
+            this.id = id;
+            this.qString = qString;
+            this.buttonsLabel = buttonsLabel;
+        }
+        public int ID { get { return this.id; } }
+        public string QString { get { return this.qString; } }
+        public List<Selection> ButtonsLabel { get { return this.buttonsLabel; } }
+
+        private int id;
+        private string qString;
+        private List<Selection> buttonsLabel;
+    }
+    public enum BUTTON_POSIOION
+    {
+        left,
+        right,
+        mid,
+        undo
+    }
+}
---
+++ OneBIOS_SKUID_Selection/StringData.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneBIOS_SKUID_Selection
+{
+
+    public static class StringData{
+        const int NO_ANS = -1;
+        public static List<List<Selection>> getButtonData
+        {
+            get
+            {
+                return new List<List<Selection>>() { 
+                new List<Selection>(){ new Selection("FreeDOS",-1,0),new Selection("Windows",1,NO_ANS),new Selection("Linux",-1,13)}, 
+                new List<Selection>(){ new Selection("Single load",1,NO_ANS),new Selection("Dual load",7,NO_ANS)},
+                new List<Selection>(){ new Selection("Win7",1,NO_ANS),new Selection("Win10 Pro",5,NO_ANS) ,new Selection("Win10",4,NO_ANS)},
+                new List<Selection>(){ new Selection("EFI",1,NO_ANS),new Selection("Lagacy",2,NO_ANS)},
+                new List<Selection>(){ new Selection("RAID",-1,1),new Selection("AHCI",-1,2)},//q5
+                new List<Selection>(){ new Selection("RAID",-1,3),new Selection("AHCI",-1,4)},//q5
+                new List<Selection>(){ new Selection("RAID",-1,5),new Selection("AHCI",-1,6)},//q4
+                new List<Selection>(){ new Selection("RAID",-1,7),new Selection("AHCI",-1,8)},//q4
+                new List<Selection>(){ new Selection("Win7/Win7",1,NO_ANS),new Selection("Win10/Win7",2,NO_ANS)},//q3
+                new List<Selection>(){ new Selection("RAID",-1,9),new Selection("AHCI",-1,10)},//q4
+                new List<Selection>(){ new Selection("RAID",-1,11),new Selection("AHCI",-1,12)},//q4
+            };
+            }
+        }
+
+        public static List<string> getQuestionData 
+        {
+            get
+            {
+                return new List<string>(new string[] { 
+                "Q1:FreeDOS, Linux or Windows?",
+                "Q2:Single load or Dual load?",
+                "Q3:Win7, Win10 or Win10 Pro?",
+                "Q4:Legacy or EFI?",
+                "Q5:AHCI or RAID?",
+                "Q5:AHCI or RAID?",
+                "Q4:AHCI or RAID?",
+                "Q4:AHCI or RAID?",
+                "Q3:Win7/Win7 or Win10/Win7?",
+                "Q4:AHCI or RAID?",
+                "Q4:AHCI or RAID?"
+            });
+            }   
+        }
+        public static List<string> getAnsData
+        {
+            get
+            {
+                return new List<string>(){
+                "FreeDOS/Legacy/AHCI \r\nSKU ID: 0001h \r\nFDOSMBAH.BAT",
+                "Win7 64bit/EFI/RAID \r\nSKU ID: 0006h \r\nWIN7GPRD.BAT",
+                "Win7 64bit/EFI/AHCI \r\nSKU ID: 0002h \r\nWIN7GPAH.BAT",
+                "Win7 64bit/Legacy/RAID \r\nWin7 32bit/Legacy/RAID \r\nSKU ID: 0007h \r\nWIN7MBRD.BAT",
+                "Win7 64bit/Legacy/AHCI \r\nWin7 32bit/Legacy/AHCI \r\nSKU ID: 0003h \r\nWIN7MBAH.BAT",
+                "Win10/EFI/RAID \r\nSKU ID: 0008h \r\nWINAGPRD.BAT",
+                "Win10/EFI/AHCI \r\nSKU ID: 0004h \r\nWINAGPAH.BAT",
+                "Win10 Pro/EFI/RAID \r\nSKU ID: 000Bh \r\nWAPRGPRD.BAT",
+                "Win10 Pro/EFI/AHCI \r\nSKU ID: 000Ah \r\nWAPRGPAH.BAT",
+                "Win7 64bit/Win7 32bit/Legacy/RAID \r\nSKU ID: 0007h \r\nWIN7MBRD.BAT",
+                "Win7 64bit/Win7 32bit/Legacy/AHCI \r\nSKU ID: 0003h \r\nWIN7MBAH.BAT",
+                "Win10/Win7/EFI/RAID \r\nSKU ID: 0009h \r\nWAW7GPRD.BAT",
+                "Win10/Win7/EFI/AHCI \r\nSKU ID: 0005h \r\nWAW7GPAH.BAT",
+                "Linpus Linux/EFI/AHCI \r\nSKU ID: 000Ch \r\nLNUXGPAH.BAT"
+
+            };
+            }
+        }
+        public static Dictionary<string, BUTTON_POSIOION> ButtonType
+        {
+            get
+            {
+                return new Dictionary<string, BUTTON_POSIOION>()
+                {
+                    {"LButton",BUTTON_POSIOION.left},
+                    {"RButton",BUTTON_POSIOION.right},
+                    {"MButton",BUTTON_POSIOION.mid},
+                    {"UndoButton",BUTTON_POSIOION.undo}
+                };
+            }
+        }
+
+    }
+}
